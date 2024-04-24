3007d27a3910752a63c55f0ad09297fa4bf410d0
Iguanx <Iquinadom@gmail.com>
2024-04-24 14:15:46 -0400

    Starting files

---
+++ Puzzle.h
@@ -0,0 +1,57 @@
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
+#include <iostream>
+using namespace std;
+
+enum{
+    boardSize = 9,
+};
+
+
+class Puzzle{
+    private:
+        int board[boardSize * boardSize]; //Choosing to use 1d array to store , should increase efficiency a little
+        /*
+        I've gone over more efficiency/memory stuff since I wrote this before
+        and I think that going for a 1d array, although losing some readability,
+        should perform better than a 2d array overall.
+        */
+    public:
+        Puzzle(){
+            for(int i = 0; i < boardSize; i++){
+                for(int j = 0; j < boardSize; j++){
+                    board[i*9 + j] = 0;
+                }
+            }
+        }
+        void PrintPuzzle(){
+            cout << "--------+-------+--------" << endl;
+            for(int i = 0; i < boardSize; i++){
+                for(int j = 0; j < boardSize; j++){
+                    if(j % 3 == 0){
+                        cout << "| ";
+                    }
+                    cout << board[i*9 + j] << " ";
+                }
+                cout << "|" << endl;
+            
+                if(i % 3 == 2){
+                    cout << "--------+-------+--------" << endl;
+                }
+            }
+        }
+
+        bool checkRow();
+        bool checkCol();
+        bool checkBox();
+        bool checkAll();
+
+
+        
+        
+};
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "Puzzle.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    Puzzle main;
+    main.PrintPuzzle();
+}
+
+
+// g++ -c main.cpp
+// g++ -o main.exe main.o
+// ./main.exe
